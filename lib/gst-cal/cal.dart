ef45a116eac19a66ad25211bf1f1c6c8a3ab6afd
userdixit <120080979+userdixit@users.noreply.github.com>
2023-02-27 03:14:49 +0530

    update expand

--- lib/gst-cal/cal.dart
+++ lib/gst-cal/cal.dart
@@ -8,170 +8,143 @@ class hyy extends StatefulWidget {
 }
 
 class _hyyState extends State<hyy> {
-  TextEditingController txtno=TextEditingController();
-  double b=0;
+  TextEditingController txtno = TextEditingController();
+  double b = 0;
+
   @override
   Widget build(BuildContext context) {
-    return
-        Scaffold(
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          // enter the no
+          TextField(
+            controller: txtno,
+          ),
+          //3% ,5%,7%
+          Row(
             children: [
-              // enter the no
-              TextField(
-                controller: txtno ,
-
+              Container(
+                height: 20,
+                width: 50,
+                //child: Text('3%'),
               ),
-              //3% ,5%,7%
-              Row(
-                children: [
-                  Container(
-                    height: 20,
-                    width: 50,
-                    //child: Text('3%'),
-                  ),
-                  ElevatedButton(onPressed: () {
-                    String birth=txtno.text;
-                    double a=double.parse(birth);
-                    b=a*0.03;
-                    setState(() {
-                       Text("$b");
-                    });
-                  }, child: Text("3%")),
-                  Container(
-                    height: 20,
-                    width: 50,
-                    //child: Text("5%"),
-                  ),
-                 ElevatedButton(onPressed: () {
-                   String birth=txtno.text;
-                   double a=double.parse(birth);
-                   b=a*0.05;
-                 }, child: Text("5%")),
-                  Container(
-                    height: 20,
-                    width: 50,
-                    child: Text("12%"),
-                  ),
-                  Container(
-                    height: 20,
-                    width: 50,
-                    child: Text("18%"),
-                  ),
-                  Container(
-                    height: 20,
-                    width: 50,
-                    child: Text("28%"),
-                  ),
-                ],
+              ElevatedButton(
+                  onPressed: () {
+                    String birth = txtno.text;
+                    double a = double.parse(birth);
+                    b = a * 0.03;
+                    setState(() {});
+                  },
+                  child: Text("3%")),
+              Container(
+                height: 20,
+                width: 50,
+                //child: Text("5%"),
               ),
-              ElevatedButton(onPressed: () {
+              ElevatedButton(
+                  onPressed: () {
+                    String birth = txtno.text;
+                    double a = double.parse(birth);
+                    b = a * 0.05;
+                    setState(() {});
+                  },
+                  child: Text("5%")),
+              ElevatedButton(
+                  onPressed: () {
+                    String birth = txtno.text;
+                    double a = double.parse(birth);
+                    b = a * 0.12;
+                    setState(() {});
+                  },
+                  child: Text("12%")),
+              ElevatedButton(
+                  onPressed: () {
+                    String birth = txtno.text;
+                    double a = double.parse(birth);
+                    b = a * 0.18;
+                    setState(() {});
+                  },
+                  child: Text("18%")),
+              ElevatedButton(
+                  onPressed: () {
+                    String birth = txtno.text;
+                    double a = double.parse(birth);
+                    b = a * 0.28;
+                    setState(() {});
+                  },
+                  child: Text("28%")),
+            ],
+          ),
+          ElevatedButton(
+              onPressed: () {
                 setState(() {
                   Text("$b");
                 });
-              }, child: Text("sumil")),
-              // final peice
-              Row(
-                children: [
-                  Container(
-                    height: 50,
-                    width: 60,
-                    child: Text("F"),
-                  ),
-                  Container(
-                    height: 50,
-                    width: 100,
-                    child: Text("Final price"),
-                  ),
-                ],
-              ),
-
-              SizedBox(height: 20),
-              //no 1 to 9
+              },
+              child: Text("sumil")),
+          // final peice
+          Row(
+            children: [
               Container(
-                height: 300,
-                width: 200,
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        Container(
-                          height: 20,
-                          width: 20,
-                          color: Color(000252427),
-                          child: Text('7'),
-                        ),
-                        Container(
-                          height: 20,
-                          width: 20,
-                          color: Color(000252427),
-                          child: Text('8'),
-                        ),
-                        Container(
-                          height: 20,
-                          width: 20,
-                          color: Color(000252427),
-                          child: Text('9'),
-                        ),
-                        Container(
-                          height: 35,
-                          width: 20,
-                          color: Color(0xffFF6B00),
-                          child: Text('Ac'),
-                        ),
-                      ],
-                    ),
-                    Row(children: [
-                      Container(
-                        height: 20,
-                        width: 20,
-                        color: Color(000252427),
-                        child: Text('4'),
-                      ),
-                      Container(
-                        height: 20,
-                        width: 20,
-                        color: Color(000252427),
-                        child: Text('5'),
-                      ),
-                      Container(
-                        height: 20,
-                        width: 20,
-                        color: Color(000252427),
-                        child: Text('6'),
-                      ),
+                height: 50,
+                width: 60,
+                child: Text("F"),
+              ),
+              Container(
+                height: 50,
+                width: 100,
+                child: Text("Final price"),
+              ),
+            ],
+          ),
+          Text("${b.toInt()}"),
+          SizedBox(height: 20),
+          //no 1 to 9
+          Container(
+            height: 300,
+            width: 200,
+            child: Column(
+              children: [
+                Expanded(
+                  child: Row(
+                    children: [
+                      no(7),
+                      no(8),
+                      no(9),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Row(
+                    children: [
+                      no(4),
+                      no(5),
+                      no(6),
                       Container(
                         height: 35,
                         width: 20,
-
                         decoration: BoxDecoration(
                           color: Color(0xffFF6B00),
                         ),
                         child: Text('Ac'),
                       ),
-                    ],),
-                    Row(children: [
-                      Container(
-                        height: 20,
-                        width: 20,
-                        color: Color(000252427),
-                        child: Text('1'),
-                      ),
-                      Container(
-                        height: 20,
-                        width: 20,
-                        color: Color(000252427),
-                        child: Text('2'),
-                      ),
-                      Container(
-                        height: 20,
-                        width: 20,
-                        color: Color(000252427),
-                        child: Text('3'),
-                      ),
-                    ],),
-                    Row(children: [
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Row(
+                    children: [
+                      no(1),
+                      no(2),
+                      no(3),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Row(
+                    children: [
                       Container(
                         height: 20,
                         width: 20,
@@ -190,13 +163,23 @@ class _hyyState extends State<hyy> {
                         color: Color(000252427),
                         child: Text('.'),
                       ),
-                    ],)
-                  ],
-                ),
-              ),
-            ],
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
-        );
+        ],
+      ),
+    );
+  }
 
+  Widget no(int num) {
+    return Container(
+      height: 20,
+      width: 20,
+      color: Color(000252427),
+      child: Text('$num'),
+    );
   }
 }
